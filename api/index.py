858f74a50450de5dc6083864dfe5a8bf095e0081
yljiang <flaming_dragon29@hotmail.com>
2023-04-09 17:04:55 -0400

    endpoint

--- api/index.py
+++ api/index.py
@@ -1,10 +1,33 @@
-from flask import Flask
+import datetime
+
+from flask import Flask, render_template, request, jsonify
+# from pymongo import MongoClient
+# import os
 
 app = Flask(__name__)
+#
+# print(os.getenv('MONGO_CONNECTION'))
+# client = MongoClient(os.getenv('MONGO_CONNECTION'))
+# db = client['realEstate']
+# buildings_collection = db['nycRentControlledBuildings']
+
 
 @app.route('/')
 def home():
     return 'Hello, World!'
+
+@app.route('/getRentControlled', methods=['GET'])
+def get_rent_controlled():
+    address = request.args.get('address')
+    # Check if the address is rent-controlled
+    is_rent_controlled = False
+    # if buildings_collection.find_one({'address': address}):
+    #     is_rent_controlled = True
+
+    return jsonify({
+        'isRentControlled': is_rent_controlled,
+        'address': address
+    })
 
 @app.route('/about')
 def about():
